db27078d2ee5fafd301ce8307e97ac29be6c08a4
rtambun <richson.tambun@gmail.com>
2021-03-09 07:47:01 +0700

    First push

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # spring-pet-clinic
+
 Spring project course for the spring-pet-clinic
---
+++ src/main/java/rtambun/training/spring/petclinic/PetclinicApplication.java
@@ -0,0 +1,13 @@
+package rtambun.training.spring.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicApplication.class, args);
+    }
+
+}
