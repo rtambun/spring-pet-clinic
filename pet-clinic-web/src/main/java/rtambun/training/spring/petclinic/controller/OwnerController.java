2aea6abb450b22a1816fdf38ec53bcb087950b09
rtambun <richson.tambun@gmail.com>
2021-03-14 22:35:18 +0800

    Add owner and vet controller

---
+++ pet-clinic-web/src/main/java/rtambun/training/spring/petclinic/controller/OwnerController.java
@@ -0,0 +1,15 @@
+package rtambun.training.spring.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"","/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+
+}
