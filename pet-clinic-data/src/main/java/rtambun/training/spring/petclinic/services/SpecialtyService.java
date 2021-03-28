fe37488039f220bb67346e41e407d3e60a1f13ac
rtambun <richson.tambun@gmail.com>
2021-03-28 16:55:24 +0800

    Add specialty service and map service

---
+++ pet-clinic-data/src/main/java/rtambun/training/spring/petclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package rtambun.training.spring.petclinic.services;
+
+import rtambun.training.spring.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
